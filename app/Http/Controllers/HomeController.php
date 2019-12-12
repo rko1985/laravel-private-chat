1b2a9c5dfcbc3c4209a0995b8a9bbab650903455
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-12-12 08:53:27 -1000

    created models, controller and resource

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        return UserResource::collection(User::all());
         return view('home');
     }
 }
