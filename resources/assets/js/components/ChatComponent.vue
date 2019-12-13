c36b2a51c9e50d1256bfb2eda0ae30be52076a1a
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-12-13 11:26:22 -1000

    created vue message component

--- resources/assets/js/components/ChatComponent.vue
+++ resources/assets/js/components/ChatComponent.vue
@@ -12,14 +12,16 @@
                 </div>
             </div>
             <div class="col-md-9">
-
+                <message-component></message-component>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import MessageComponent from './MessageComponent';
     export default {
+        components: {MessageComponent},
         mounted() {
             console.log('Component mounted.')
         }
---
+++ resources/assets/js/components/MessageComponent.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="card card-default chat-box">
+        <div class="card-header">
+            Chats
+        </div>
+        <div class="card-body">
+            Hey how are you?
+        </div>
+        <form class="card-footer">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Write your message here">
+            </div>
+            
+        </form>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.chat-box{
+    height: 400px;
+}
+</style>
