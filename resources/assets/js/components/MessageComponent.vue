e16844d97fb7c850af8ce02f09725fef5b0cdda6
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-12-16 09:31:46 -1000

    installed vue chat scroll

--- resources/assets/js/components/MessageComponent.vue
+++ resources/assets/js/components/MessageComponent.vue
@@ -4,10 +4,12 @@
         <div class="card-header">
             Chats
         </div>
-        <div class="card-body">
-            Hey how are you?
+        <div class="card-body" v-chat-scroll>
+            <p class="card-text" v-for="chat in chats" :key="chat.message">
+                {{chat.message}}
+            </p>
         </div>
-        <form class="card-footer">
+        <form class="card-footer" @submit.prevent="send">
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Write your message here">
             </div>
@@ -19,12 +21,47 @@
 
 <script>
 export default {
-    
+    data(){
+      return {
+          chats: []
+      }      
+    },
+    methods:{        
+        send(){
+            console.log('yeahhhhh')
+        }
+    },
+    created(){
+        this.chats.push(
+        {message:'Heyy'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you?'},
+        {message:'how are you? last'},
+        )
+    }
 }
 </script>
 
 <style>
 .chat-box{
     height: 400px;
+}
+
+.card-body{
+    overflow-y: scroll;
 }
 </style>
